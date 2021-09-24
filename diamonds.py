fc770b07a34cba797156ec6cb9186d5cd3a02c07
JoshDonato <80470979+JoshDonato@users.noreply.github.com>
2021-09-24 16:41:19 -0400

    Create diamonds.py

---
+++ diamonds.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(1, 1+n): #all diamonds
+	for j in range(1, 1+ n - 2 * abs(i - int((n+1)/2))): #columns of each diamond
+		m = n - 2 * abs(i - int((n+1)/2))
+		for k in range(abs(int((m+1) / 2) - j)):
+			print(" ",end="")
+		for k in range(m - 2 * abs(j - int((m+1)/2))):
+			print("*",end="")
+		for k in range(abs(int((m+1) / 2) - j)):
+			print(" ",end="")
+		print("")
+	print("")
